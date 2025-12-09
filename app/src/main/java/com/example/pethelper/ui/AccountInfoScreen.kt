8c94e5c41e8a8459a3dd9bfc3d10a2e2211e1c61
Egot_Zhavoronkov <georgelarkenston@gmail.com>
2025-12-09 18:13:30 +0300

    структура и основные файлы

--- app/src/main/java/com/example/pethelper/ui/AccountInfoScreen.kt
+++ app/src/main/java/com/example/pethelper/ui/AccountInfoScreen.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.pethelper.R
 
 data class Pet(
     val id: Int,
@@ -78,7 +79,7 @@ fun AccountInfoScreen(
             ) {
                 // Аватар
                 Image(
-                    painter = painterResource(id = avatarResId),
+                    painter = painterResource(R.drawable.cot),
                     contentDescription = "Аватар пользователя",
                     modifier = Modifier
                         .size(120.dp)
